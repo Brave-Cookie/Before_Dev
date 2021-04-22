d77ca8d3c656f45e0851ff3945c5098f51a7dbb0
Suji Kim <bion239@naver.com>
2021-04-22 17:27:29 +0900

    [AI] refactor: n_mfcc=36, kernel=linear로 수정

--- before_dev/Ray/SVM/svm_implement.py
+++ before_dev/Ray/SVM/svm_implement.py
@@ -39,21 +39,13 @@ from sklearn.metrics import accuracy_score
 # 학습 데이터와 훈련 데이터 split
 X_train, X_test, y_train, y_test = train_test_split(dataset, y, test_size = 0.2, random_state = 42, shuffle = True)
 
-# 커널을 설정하고(필요시 인자 수정: C, gamma)
+# 커널 설정
 clf = SVC(kernel = 'linear', probability=True)
 
 
-clf.fit(X_train, y_train)  
-
-clf.predict(X_test)
-print(accuracy_score(clf.predict(X_test), y_test))
-print("----")
-
-# 최적화
-clf = SVC(kernel = 'rbf', C = 1, probability=True)
-
 clf.fit(X_train, y_train)
-print("----")
+
+# 정확도 약 93%
 print(accuracy_score(clf.predict(X_test), y_test))
 
 
