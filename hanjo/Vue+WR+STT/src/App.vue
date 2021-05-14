cf0bcdaec98e0ddc108192054663caf3caa5f462
한재원 <hanjo8813@gmail.com>
2021-05-15 01:41:50 +0900

    [BE] test : start Vue + Web RTC #56

---
+++ hanjo/Vue+WR+STT/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+    <div id="app">
+      <div id="header">
+        <component :is="getIsAuth() ? 'HeaderAuth' : 'Header'"></component> 
+      </div>
+    <br>
+  <router-view>
+  </router-view>
+  </div>
+
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import HeaderAuth from './components/HeaderAuth.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Header, 
+    HeaderAuth
+  },
+  methods: {
+    getIsAuth() {
+      if(localStorage.accessToken){
+        return 1;
+      }
+      else{
+        return 0;
+      }
+      
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+
+#header a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#header a.router-link-exact-active {
+  color: #7b68ee;
+}
+
+</style>
