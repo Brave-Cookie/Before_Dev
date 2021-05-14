bf1cad910a8bb35f9f4e3e8d6aec006c8796934f
jeonho <jhkang1995@naver.com>
2021-05-14 17:21:29 +0900

    [BE] 플라스크 ORM 연동

--- jeonbar2/Flask_Test/app.py
+++ jeonbar2/Flask_Test/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify, current_app
 import sqlalchemy
 app = Flask(__name__)
 import config
+
 from sqlalchemy import create_engine
 app.config.from_pyfile("config.py")
 app.config['SQLALCHEMY_DATABASE_URL'] = config.DB_URL
@@ -10,6 +11,7 @@ app.database = database
 
 connection = database.connect()
 metadata = sqlalchemy.MetaData()
+
 table = sqlalchemy.Table('user_info',metadata,autoload=True,autoload_with=database)
 print(table.columns.keys())
 
@@ -17,10 +19,13 @@ query = sqlalchemy.select([table])
 
 # 이때 query의 내용을 출력해보면 sql query인 것을 알 수 있음
 print(query)
-
-# 쿼리 실행
 result_proxy = connection.execute(query)
 result_set = result_proxy.fetchall()
+
+query = sqlalchemy.insert(table).values(user_id='내가',user_pw='이걸',user_email='했네',user_name='s') # 이때 values는 table의 column의 순서와 갯수가 일치해야 함
+result_proxy = connection.execute(query)
+result_proxy.close()
+# 쿼리 실행
 
 # 결과 print 이때 10개만 출력하도록 함. 단순한 set 자료구조의 형태를 하고 있음.
 print(result_set)
