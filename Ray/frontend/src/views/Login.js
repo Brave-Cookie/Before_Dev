22f57c4e77c2e14ee2aeec54173ea3d49d72325e
Suji Kim <bion239@naver.com>
2021-05-18 01:24:44 +0900

    [FE] test: create react project

---
+++ Ray/frontend/src/views/Login.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import Axios from 'axios'
+
+
+class Login extends Component {
+    render() {
+
+
+
+        return(
+            <div>
+                <h3>로그인</h3>
+                <form>
+                    <input id="user_id" name="user_id" placeholder="ID" /> <br></br>
+                    <input id="user_pw" name="user_pw" type="password" placeholder="Password" /> <br></br>
+                
+                    <button>로그인</button>
+                </form>
+                <br></br>
+                <Link to="/signup">회원가입</Link>
+            </div>
+        )
+    }
+}
+
+export default Login;
